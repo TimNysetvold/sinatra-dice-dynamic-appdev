e6f0a74af86895eafe7d7de36fe13322ae8e071a
TimNysetvold <tim.nysetvold@gmail.com>
2024-05-02 22:08:43 +0000

    Finished

--- app.rb
+++ app.rb
@@ -5,50 +5,17 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
+get("/dice/:number_of_dice/:dice_sides") do
   @rolls = []
 
-  2.times do
-    die = rand(1..6)
+  @count_dice = params.fetch("number_of_dice").to_i
+  @side_dice = params.fetch("dice_sides").to_i
+
+  @count_dice.times do
+    die = rand(1..@side_dice)
 
     @rolls.push(die)
   end
 
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
+  erb(:flexible)
 end
